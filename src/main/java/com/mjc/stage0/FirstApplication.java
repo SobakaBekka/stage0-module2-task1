89dd6404b9c76a1c55b11cd5740dff6c60692e05
SobakaBekka <st69704@upce.cz>
2024-02-15 15:38:49 +0100

    Update the task for output in console Helo, World and add the comment

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,7 +2,7 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-
+//Writing first Application
         System.out.println("Hello, World!");
 
     }
